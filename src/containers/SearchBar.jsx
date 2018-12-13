ce15404fb31fa50d40cef7273aa6b2fbed9f2dcc
Neil Reyes <rayneilreyes@gmail.com>
2018-12-13 17:52:53 +0800

    Added methods for onSubmit and onChange event

--- src/containers/SearchBar.jsx
+++ src/containers/SearchBar.jsx
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            term: '',
+        }
+
+        this.handleOnSubmit = this.handleOnSubmit.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event){
+        this.setState({
+            term: event.target.value,
+        });
+    }
+
+    handleOnSubmit(event){
+        event.preventDefault();
+    }
+
     render(){
         return(
-            <form className="input-group">
-                <input />
+            <form
+                className="input-group"
+                onSubmit={this.handleOnSubmit}
+            >
+                <input
+                    className="form-control"
+                    name="term"
+                    placeholder="Give a five-day forecast in your favorite cities."
+                    onChange={this.handleInputChange}
+                    value={this.state.term}
+                />
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-primary">Search City</button>
+                </span>
             </form>
         );
     }
