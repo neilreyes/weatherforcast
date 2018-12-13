20163b9991a0311d9b10613a7efb700ec4fd8e2c
Neil Reyes <rayneilreyes@gmail.com>
2018-12-13 17:31:11 +0800

    Added basic smart component - SearchBar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import '../App.css';
 
+// Components
+import SearchBar from '../containers/SearchBar';
+
 const App = () =>{
-    return(<div>Starter boilerplate</div>)
+    return(
+        <div className="container">
+            Starter boilerplate
+            <SearchBar />
+        </div>
+    )
 }
 
 export default App;
